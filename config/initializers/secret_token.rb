f992507391f82392651e9ae7ddd1e2f757eda4a0
EC2 Default User <ec2-user@ip-10-35-129-223.ec2.internal>
2013-11-29 03:00:09 +0000

    slf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyTest::Application.config.secret_token = '631c7da002db9f0dde3b1716ae5d186ff1af85f5b94437ee9fc4c5d76fb193f4e2ef384353ca0cf68f52d7acdbe03e77dda6cdaec440b3916408afa01d3016a2'
